e4b4e277a1528d05e66f9b9970932a58b8e3625a
TudoseVlad <69509086+TudoseVlad@users.noreply.github.com>
2024-10-11 15:23:43 +0300

    new project

--- src/main/java/com/example/spring_project/SpringProjectApplication.java
+++ src/main/java/com/example/spring_project/SpringProjectApplication.java
@@ -1,13 +1,13 @@
-package com.example.Project;
+package com.example.spring_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class SpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(SpringProjectApplication.class, args);
 	}
 
 }
